ad0848727f68cf81704714f7bc6bb32c9c4569d4
morgan hoare <hoaremorgan@gmail.com>
2020-02-21 11:44:10 -0300

    Initial commit

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,30 @@
+class DosesController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @ingredients = Ingredient.all
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    # we need `cocktail_id` to associate dose with corresponding cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :amount, :ingredient_id)
+  end
+end
